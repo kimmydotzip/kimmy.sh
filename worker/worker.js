7bc0c5a0c4474a3e904038fe5e23025c7b9f72d8
kimmy.zip <986201+kimmydotzip@users.noreply.github.com>
2025-11-25 14:10:08 -0500

    reorganized web files and added worker script

---
+++ worker/worker.js
@@ -0,0 +1,15 @@
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  const userAgent = request.headers.get("User-Agent") || "";
+
+  if (userAgent.toLowerCase().includes("curl")) {
+    // Simply forward the request to the Cloudflare Pages backend
+    return fetch(request);
+  } else {
+    // Redirect browsers to https://kimmy.zip
+    return Response.redirect("https://kimmy.zip", 302);
+  }
+}
